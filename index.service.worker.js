098a9bbdcd2d0e68de24b267374ba4480ea59d77
HelVal1010 <valenciayonghel@gmail.com>
2024-09-17 14:38:13 +0800

    wat

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1726554719|19061529164";
+const CACHE_VERSION = "1726555056|19398614796";
 const CACHE_PREFIX = "Nds-Sdc-Proj-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
